9a485b23fcb797146e1f1fbdff3d1faa6501c731
kenjirohayashi <kenjiro.rugby@icloud.com>
2021-08-08 22:15:16 +0900

    initial commit

---
+++ src/components/Header.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import logo from './logo.svg';
+
+const Header = () =>{
+
+  return(
+    <header className="App-header">
+      <img src={logo} className="App-logo" alt="logo" />
+      <p>
+        Edit <code>src/App.tsx</code> and save to reload.
+      </p>
+    </header>
+  )
+};
+
+
+export default Header;
+
